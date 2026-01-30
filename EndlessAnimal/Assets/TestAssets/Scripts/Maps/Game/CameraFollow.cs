88a45551ba5e9aa16cd9ce9690e5ab5feba8ccc4
อิทธิพัทธ์ จันทร์ขุน <phat4725@gmail.com>
2026-01-30 23:01:52 +0700

    Create Menu scene & separate test assets

---
+++ EndlessAnimal/Assets/TestAssets/Scripts/Maps/Game/CameraFollow.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    [Header("Offset")]
+    public Vector3 offset = new Vector3(0, 8, -12);
+
+    [Header("Smooth")]
+    public float smoothSpeed = 5f;
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        Vector3 desiredPos = target.position + offset;
+        transform.position = Vector3.Lerp(
+            transform.position,
+            desiredPos,
+            smoothSpeed * Time.deltaTime
+        );
+
+        transform.LookAt(target);
+    }
+}
